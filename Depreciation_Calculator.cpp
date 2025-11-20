313ad84f7d508a65027f68bb09a145b2eceee5cb
Cole Puckett <cjp7e@mtmail.mtsu.edu>
2025-11-20 11:58:46 -0600

    feat: project skeleton and comments throughout

---
+++ Depreciation_Calculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <print>
+#include <string>
+
+
+
+int main(){
+
+    // prompt the user for cost, estimated salvage value, and estimated useful life
+    // ask if user would like to estimate units of production depreciation method as well
+    // if yes
+        // prompt for units of production throughout the years
+
+    // store in variables
+
+    // calculate straight line depreciation and
+    // output a timeline for over the years
+
+    // calculate double declining balance and
+    // output a timeline for over the years
+
+    // calculate units of production depreciation
+    // output a timeline showcasing it
+
+
+    return 0;
+}
